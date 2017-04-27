b17d1f760ff6060baeb6ff63d54830fdb9db3431
Mario Miklos <mxm1021@rit.edu>
2017-04-28 01:37:52 +0200

    still working on the search

--- src/sample/DetailedBook.java
+++ src/sample/DetailedBook.java
@@ -1,21 +1,25 @@
 package sample;
 
+import javafx.beans.property.SimpleStringProperty;
+
 import java.util.ArrayList;
 
 /**
  * @author Boyz
  */
+
+// ArrayList autora u jedan dugi string TODO
 public class DetailedBook {
-    private String book_isbn;
-    private String book_title;
+    private SimpleStringProperty book_isbn;
+    private SimpleStringProperty book_title;
     private int book_publisher_year;
     private int book_copies;
-    private String book_location;
+    private SimpleStringProperty book_location;
     private int book_genre;
-    private String publisher_name;
-    private ArrayList<String> authors;
+    private SimpleStringProperty publisher_name;
+    private ArrayList<SimpleStringProperty> authors;
 
-    public DetailedBook(String book_isbn, String book_title, int book_publisher_year, int book_copies, String book_location, int book_genre, String publisher_name, ArrayList<String> authors) {
+    public DetailedBook(SimpleStringProperty book_isbn, SimpleStringProperty book_title, int book_publisher_year, int book_copies, SimpleStringProperty book_location, int book_genre, SimpleStringProperty publisher_name, ArrayList<SimpleStringProperty> authors) {
         this.book_isbn = book_isbn;
         this.book_title = book_title;
         this.book_publisher_year = book_publisher_year;
@@ -30,19 +34,19 @@ public class DetailedBook {
         return book_genre;
     }
 
-    public String getPublisher_name() {
+    public SimpleStringProperty getPublisher_name() {
         return publisher_name;
     }
 
-    public ArrayList<String> getAuthor() {
+    public ArrayList<SimpleStringProperty> getAuthor() {
         return authors;
     }
 
-    public String getBook_isbn() {
+    public SimpleStringProperty getBook_isbn() {
         return book_isbn;
     }
 
-    public String getBook_title() {
+    public SimpleStringProperty getBook_title() {
         return book_title;
     }
 
@@ -54,7 +58,7 @@ public class DetailedBook {
         return book_copies;
     }
 
-    public String getBook_location() {
+    public SimpleStringProperty getBook_location() {
         return book_location;
     }
 
