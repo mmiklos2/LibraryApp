08f4d79ce4d37490647863ecf0938dff1adcc996
Ivan Mazalovic <ixm5331@rit.edu>
2017-05-06 02:45:56 +0200

    commiitooooooo

--- src/sample/PDFBuilder.java
+++ src/sample/PDFBuilder.java
@@ -13,99 +13,98 @@ import java.util.List;
 public class PDFBuilder {
 
 
-    private static Font catFont = new Font(Font.FontFamily.TIMES_ROMAN, 18,
-            Font.BOLD);
-    private static Font redFont = new Font(Font.FontFamily.TIMES_ROMAN, 12,
-            Font.NORMAL, BaseColor.RED);
-    private static Font subFont = new Font(Font.FontFamily.TIMES_ROMAN, 16,
-            Font.BOLD);
-    private static Font smallBold = new Font(Font.FontFamily.TIMES_ROMAN, 12,
-            Font.BOLD);
-    File f= new File( "AllBooks.pdf");
-    int counter=0;
-    private List<DetailedBook> dba=new ArrayList<DetailedBook>() {
+    private static Font fontTimes = new Font(Font.FontFamily.TIMES_ROMAN, 18, Font.BOLD);
+
+    private File f = new File("AllBooks.pdf");
+    private int counter = 0;
+
+    private List<DetailedBook> dba = new ArrayList<DetailedBook>() {
     };
-    public PDFBuilder(List<DetailedBook> _dba){
-        dba=_dba;
+
+    public PDFBuilder(List<DetailedBook> _dba) {
+        dba = _dba;
     }
-   private  void createTable(Section subCatPart)
+
+    private void createTable(Section sec)
             throws BadElementException {
 
         PdfPTable table = new PdfPTable(6);
 
-        PdfPCell c1 = new PdfPCell(new Phrase("Book ISBN "));
-        c1.setHorizontalAlignment(Element.ALIGN_CENTER);
-        table.addCell(c1);
+        PdfPCell cell = new PdfPCell(new Phrase("Book ISBN "));
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        table.addCell(cell);
 
-        c1 = new PdfPCell(new Phrase("Book Title"));
-        c1.setHorizontalAlignment(Element.ALIGN_CENTER);
-        table.addCell(c1);
+        cell = new PdfPCell(new Phrase("Book Title"));
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        table.addCell(cell);
 
-       c1 = new PdfPCell(new Phrase("Book Author"));
-       c1.setHorizontalAlignment(Element.ALIGN_CENTER);
-       table.addCell(c1);
+        cell = new PdfPCell(new Phrase("Book Author"));
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        table.addCell(cell);
 
-        c1 = new PdfPCell(new Phrase("Book Publisher"));
-        c1.setHorizontalAlignment(Element.ALIGN_CENTER);
-        table.addCell(c1);
+        cell = new PdfPCell(new Phrase("Book Publisher"));
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        table.addCell(cell);
 
-       c1 = new PdfPCell(new Phrase("Year of publication"));
-       c1.setHorizontalAlignment(Element.ALIGN_CENTER);
-       table.addCell(c1);
+        cell = new PdfPCell(new Phrase("Year of publication"));
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        table.addCell(cell);
 
-       c1 = new PdfPCell(new Phrase("Book Genre"));
-       c1.setHorizontalAlignment(Element.ALIGN_CENTER);
-       table.addCell(c1);
+        cell = new PdfPCell(new Phrase("Book Genre"));
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        table.addCell(cell);
 
         table.setHeaderRows(1);
 
-       for (int i=0;i<dba.size();i++){
-           table.addCell(String.valueOf(dba.get(i).getBook_isbn().getValue()));
-           table.addCell(String.valueOf(dba.get(i).getBook_title().getValue()));
-           table.addCell(String.valueOf(dba.get(i).getAuthor().getValue()));
-           table.addCell(String.valueOf(dba.get(i).getPublisher_name().getValue()));
-           table.addCell(String.valueOf(dba.get(i).getBook_publisher_year().getValue()));
-           table.addCell(String.valueOf(dba.get(i).getBook_genre().getValue()));
+        for (int i = 0; i < dba.size(); i++) {
+            table.addCell(String.valueOf(dba.get(i).getBook_isbn().getValue()));
+            table.addCell(String.valueOf(dba.get(i).getBook_title().getValue()));
+            table.addCell(String.valueOf(dba.get(i).getAuthor().getValue()));
+            table.addCell(String.valueOf(dba.get(i).getPublisher_name().getValue()));
+            table.addCell(String.valueOf(dba.get(i).getBook_publisher_year().getValue()));
+            table.addCell(String.valueOf(dba.get(i).getBook_genre().getValue()));
 
-       }
-        subCatPart.add(table);
+        }
+        sec.add(table);
     }
 
-    private  void addMetaData(Document document) {
+    private void addMetaData(Document document) {
         document.addTitle("All_Books");
         document.addSubject("Books in Library");
         document.addKeywords("Java, PDF");
         document.addAuthor("ZG4 Team");
         document.addCreator("ZG4 Team");
     }
-    private  void addContent(Document document) throws DocumentException {
-        Anchor anchor = new Anchor("Books in Library", catFont);
+
+    private void addContent(Document document) throws DocumentException {
+        Anchor anchor = new Anchor("Books in Library", fontTimes);
         anchor.setName("Books in Library");
-        Chapter catPart = new Chapter(new Paragraph(anchor),1);
+        Chapter catPart = new Chapter(new Paragraph(anchor), 1);
 
         Paragraph subPara = new Paragraph();
-        Section subCatPart = catPart.addSection(subPara);
+        Section sec = catPart.addSection(subPara);
 
-        createTable(subCatPart);
+        createTable(sec);
         document.add(catPart);
     }
+
     public void populatePDF() {
 
-        if (f.exists()){
+        if (f.exists()) {
 
-        try {
-            Document document = new Document();
-            PdfWriter.getInstance(document, new FileOutputStream("AllBooks_Copy"+"("+counter+")"+".pdf"));
-            document.open();
-            addMetaData(document);
-            addContent(document);
-            document.close();
-            f= new File("AllBooks_Copy"+"("+counter+")"+".pdf");
-            counter++;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }else{
+            try {
+                Document document = new Document();
+                PdfWriter.getInstance(document, new FileOutputStream("AllBooks_Copy" + "(" + counter + ")" + ".pdf"));
+                document.open();
+                addMetaData(document);
+                addContent(document);
+                document.close();
+                f = new File("AllBooks_Copy" + "(" + counter + ")" + ".pdf");
+                counter++;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
             try {
                 Document document = new Document();
                 PdfWriter.getInstance(document, new FileOutputStream("AllBooks.pdf"));
